4ed5a562edeadacc919f25878989bfda3224a7bd
future81 <future81@users.noreply.github.com>
2021-07-22 04:52:13 +0200

    Add files via upload

---
+++ python-helloworld/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import json
+import logging
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    app.logger.info("Hello Request Was Successful")
+    return "Hello World!"
+@app.route("/status")
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info("Status Request Successful")
+    return response
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data": {"UserCount": 140, "UserCountActive": 23}}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info("Metrics Request Successful")
+    return response
+if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    app.run(host='0.0.0.0')
